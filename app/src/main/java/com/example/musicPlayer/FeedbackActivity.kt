65eb28e148765d9727b4915ebf1c4aa25c5e022d
Nham <100785756+luongphungnham@users.noreply.github.com>
2024-05-01 18:50:00 +0700

    Initail commit

--- app/src/main/java/com/example/musicPlayer/FeedbackActivity.kt
+++ app/src/main/java/com/example/musicPlayer/FeedbackActivity.kt
@@ -1,6 +1,7 @@
 package com.example.musicPlayer
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +18,11 @@ class FeedbackActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(binding.root)
         supportActionBar?.title = "Feedback"
+        val feedbackMsg = binding.feedbackMsgFA.text.toString() + "\n" + binding.emailFA.text.toString()
+        binding.topicFA.text.toString()
+        binding.sendFA.setOnClickListener {
+        Toast.makeText(this, feedbackMsg, Toast.LENGTH_LONG).show()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
